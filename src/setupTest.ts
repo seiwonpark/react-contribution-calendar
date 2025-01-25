de6690faa700a609220ca6d97ed11f078f150ff3
Seiwon Park <seiwonpark@Seiwons-MacBook-Pro.local>
2025-01-26 01:33:20 +0900

    fix. resolve jsdom issue

--- src/setupTest.ts
+++ src/setupTest.ts
@@ -1,2 +1,9 @@
 /// <reference types="vitest/globals" />
 import '@testing-library/jest-dom/vitest'
+
+/**
+ * For handling `ReferenceError: structuredClone is not defined` jsdom issue
+ */
+global.structuredClone = (val) => {
+  return JSON.parse(JSON.stringify(val))
+}
