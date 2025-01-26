278ae0d750c8a5f22cce8857d391530478d524a5
seiwonpark <psw7347@gmail.com>
2025-01-26 16:46:04 +0900

    fix. bump workflow node version from 16 to 18

--- src/setupTest.ts
+++ src/setupTest.ts
@@ -1,9 +1,2 @@
 /// <reference types="vitest/globals" />
 import '@testing-library/jest-dom/vitest'
-
-/**
- * For handling `ReferenceError: structuredClone is not defined` jsdom issue
- */
-global.structuredClone = (val) => {
-  return JSON.parse(JSON.stringify(val))
-}
